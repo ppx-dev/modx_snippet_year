0b39491693a56c402e07e10e869f26bafbf3abb1
ppx-dev <dev@parapix.ru>
2017-03-14 14:52:38 +0400

    edit

--- dev.php
+++ dev.php
@@ -1,1 +1,5 @@
-<?php if ($modx->user->hasSessionContext('mgr')) {$output = '<div style="position: fixed; bottom: 5px; right: 5px; color: black; text-align: left; background: white; padding: 10px; z-index: 999; font-size: 14px; line-height: 1.3;">Время:   [^t^]<br>Память:   '.round((memory_get_peak_usage(true) / 1024 / 1024),2).' Мб<br>Источник:   [^s^]<br>Запросы:   [^q^]<br>Время базы:   [^qt^]</div>';}return $output;
+<?php
+if ($modx->user->hasSessionContext('mgr')) {
+$output = '<div style="position: fixed; bottom: 5px; right: 5px; color: black; text-align: left; background: white; padding: 10px; z-index: 999; font-size: 14px; line-height: 1.3;">Время:   [^t^]<br>Память:   '.round((memory_get_peak_usage(true) / 1024 / 1024),2).' Мб<br>Источник:   [^s^]<br>Запросы:   [^q^]<br>Время базы:   [^qt^]</div>';
+}
+return $output;
