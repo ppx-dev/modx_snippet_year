df858b96a2358ec0bd2c0641583a4473363c6ece
ppx-dev <dev@parapix.ru>
2017-03-15 17:16:17 +0400

    Create numformat.php

---
+++ numformat.php
@@ -0,0 +1,5 @@
+<?php
+//[[*price:numformat]]
+if(strlen($input)==0) return '';
+$input = floatval(str_replace(array(' ',','), array('','.'), $input));
+return number_format($input,(floor($input) == $input ? 0 : 2),'.',' ');
